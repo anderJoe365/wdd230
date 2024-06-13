4205f4316ea38a65f21c1ccc9cc037dfe8f2badf
anderJoe365 <anderson.joseph3656@gmail.com>
2024-06-13 12:01:09 -0600

    fixing alignment, Getting the copyright set up

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,2 +1,7 @@
 let lastMod = new Date(document.lastModified);
 document.querySelector('#lastModified').textContent = lastMod;
+
+const option = {
+    year: 'numeric'
+};
+document.querySelector('#yearDate').innerHTML = new Date().toLocaleDateString("en-US", option);
